8864d6b02f2e34ff1e855d684fd6ffab954a24ff
UnMugViolet <jaguinpaul@gmail.com>
2024-03-19 16:39:30 +0100

    Added the routes for project

--- src/router/index.js
+++ src/router/index.js
@@ -10,13 +10,43 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
+      path: '/a-propos',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    // {
+    //   path: '/signup',
+    //   name: 'signup',
+    //   component: () => import('../views/SignupView.vue')
+    // },
+    // {
+    //   path: '/login',
+    //   name: 'login',
+    //   component: () => import('../views/LoginView.vue')
+    // },
+    // {
+    //   path: '/projet',
+    //   name: 'project',
+    //   component: () => import('../views/ProjectView.vue')
+    // },
+    // {
+    //   path: '/projet/:id',
+    //   name: 'project',
+    //   component: () => import('../views/ProjectIdView.vue')
+    // },
+    // {
+    //   path: '/evenement',
+    //   name: 'event',
+    //   component: () => import('../views/EventView.vue')
+    // }
+    // {
+    //   path: '/evenement/:id',
+    //   name: 'event',
+    //   component: () => import('../views/EventIdView.vue')
+    // }
   ]
 })
 
