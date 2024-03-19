5466a3a1fac6f5c204bddeaff1cb7bde9990975d
UnMugViolet <jaguinpaul@gmail.com>
2024-03-19 17:05:18 +0100

    Added header and remove useless style

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+
+import router from '@/router';
+<template>
+  <header class="absolute flex w-full justify-between p-12">
+    <div>
+      <router-link to="/">
+        <img src="@/assets/logo.svg" alt="Vue logo" class="logo" width="125" height="125" />
+      </router-link>
+    </div>
+    <nav class="flex gap-2">
+      <router-link to="/a-propos">A propos</router-link>
+      <router-link to="/contact">Nous contacter</router-link>
+    </nav>
+    <div class="flex items-center gap-1">
+      <p>Contact</p>
+      <img src="@/assets/user.svg" alt="User Icon" class="w-10 h-10" />
+    </div>
+  </header>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     // {
+    //   path: '/contact',
+    //   name : 'contact',
+    //   component: () => import('../views/ContactView.vue')
+    // }
+    // {
     //   path: '/signup',
     //   name: 'signup',
     //   component: () => import('../views/SignupView.vue')
