c67bb4878b47cc1c4172b9b3608eb1f4bf823537
BaptisteO1 <baptiste.mr01@gmail.com>
2024-03-20 11:19:29 +0100

    Ajout des composents Hero et ThinMainButton

---
+++ src/components/ThinMainButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <button class="border border-white rounded-md py-2 px-8 mt-4">{{props.textButton}}</button>
+</template>
+
+<script setup>
+const props = defineProps(['textButton']);
+</script>
