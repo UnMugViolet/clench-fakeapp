b3792aa0e166c1d78ee4bc7d791d9ef5c4ab1966
BaptisteO1 <baptiste.mr01@gmail.com>
2024-03-20 11:20:11 +0100

    Ajout de la page Evènement et de ses composants

---
+++ src/views/EventView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col 2xl:items-center px-24">
+    <Hero />
+    <div class="flex flex-col gap-10">
+      <EventContainerCard title-event="Les évènements à venir"/>
+      <EventContainerCard title-event="Les évènements passés"/>
+    </div>
+  </div>
+</template>
+  
+<script setup>
+    import Hero from "@/components/Hero.vue";
+    import EventContainerCard from "@/components/EventContainerCard.vue";
+</script>
+
+<style>
+
+  .container_card {
+    scrollbar-color: white transparent;
+    scrollbar-width: thin;
+  }
+
+</style>
