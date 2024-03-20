1ee000c57cb6a1dc224b5c1651193b8b56d07a0d
UnMugViolet <jaguinpaul@gmail.com>
2024-03-20 17:45:00 +0100

    🔥 Remove files

--- src/components/Hero.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="flex flex-col items-center gap-6 pt-12 pb-24 text-center">
-        <p>L’application qui facilite votre vie éstudiantine</p>
-        <h1 class="text-5xl font-bold">Vivez au rythme de votre Campus</h1>
-        <p class="text-2xl font-medium w-7/12">Explorez la vie de votre campus d'une manière totalement nouvelle grâce à une expérience unique et immersive.</p>
-    </div>
-</template>
