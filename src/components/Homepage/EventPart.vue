daa7f53e37d99234df8f87f455f20e479be1e619
UnMugViolet <jaguinpaul@gmail.com>
2024-03-21 12:03:19 +0100

    ⚡️ Carroussel is done

--- src/components/Homepage/EventPart.vue
+++ src/components/Homepage/EventPart.vue
@@ -4,17 +4,43 @@
   import CarouselItem from "@/components/ui/carousel/CarouselItem.vue";
   import CarouselPrevious from "@/components/ui/carousel/CarouselPrevious.vue";
   import CarouselNext from "@/components/ui/carousel/CarouselNext.vue";
+  import Button from "@/components/ui/button/Button.vue";
+  import ProjectCard from "@/components/Homepage/ProjectsCard.vue";
 </script>
 
 <template>
-  <h2 class="text-3xl font-semibold">Découvrez nos projets</h2>
+  <h2 class="text-3xl font-semibold mb-11">Découvrez nos projets</h2>
   <div class="flex w-full gap-20">
     <div class="w-1/2">
       <Carousel>
         <CarouselContent>
-          <CarouselItem>...</CarouselItem>
-          <CarouselItem>...</CarouselItem>
-          <CarouselItem></CarouselItem>
+          <CarouselItem>
+            <ProjectCard 
+              title="@beuverie"
+              description="The React Framework - created and maintained by @vercel"
+              date ="Février 2024"
+              imageUrl="/src/assets/images/event-le-jour-et-la-nuit.png"
+              urlEvent="/evenement"
+            />
+          </CarouselItem>
+          <CarouselItem>
+            <ProjectCard 
+              title="@beuverie"
+              description="The React Framework - created and maintained by @vercel"
+              date ="Février 2024"
+              imageUrl="/src/assets/images/event-le-jour-et-la-nuit.png"
+              urlEvent="/evenement"
+            />
+          </CarouselItem>
+          <CarouselItem>
+            <ProjectCard 
+              title="@beuverie"
+              description="The React Framework - created and maintained by @vercel"
+              date ="Février 2024"
+              imageUrl="/src/assets/images/event-le-jour-et-la-nuit.png"
+              urlEvent="/evenement"
+            />
+          </CarouselItem>
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
@@ -23,8 +49,9 @@
     <div class="w-1/2 flex items-center">
       <div class="ml-3">
         <p class="mr-40 text-lg">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.</p>
-        <Button variant="default" class="mt-14">Voir les événements</Button>
+        <Button variant="outline" class="mt-14">Voir les événements</Button>
       </div>
     </div>
   </div>
 </template>
+
---
+++ src/components/Homepage/ProjectsCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex justify-center relative hover:backdrop-blur-md w-full image-size">
+    <div class="w-full h-full bg-cover bg-center rounded-md" :style="{ backgroundImage: `url(${imageUrl})` }"></div>
+    <div class="absolute bottom-0 left-0 py-6 px-12 backdrop-blur-sm w-full bg-gradient-to-t from-black to-transparent rounded-md">
+      <h4 class="font-semibold mb-2">{{title}}</h4>
+      <p class="mb-1">{{description}}</p>
+      <router-link :to="urlEvent" class="flex gap-2">
+        <img src="/src/assets/icones/calendar.svg" alt="calendar-icon" />
+        <p class="text-slate-400">Rejoindre {{ date }}</p>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps([
+    'title',
+    'description',
+    'imageUrl',
+    'urlEvent',
+    'date'
+  ]);
+</script>
+
+<style scoped>
+
+  .image-size{
+    height: 45dvh;
+  }
+
+</style>
