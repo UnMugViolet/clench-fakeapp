f0801525636defa15dc25f60e25279dcf610d8c5
UnMugViolet <jaguinpaul@gmail.com>
2024-03-23 19:39:23 +0100

    Add link to event button homepage

--- src/components/Homepage/EventPart.vue
+++ src/components/Homepage/EventPart.vue
@@ -54,7 +54,11 @@
     <div class="w-1/2 flex items-center">
       <div class="ml-3">
         <p class="mr-40 text-lg">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.</p>
-        <Button variant="outline" class="mt-14">Voir les événements</Button>
+        <router-link to="/evenements">
+          <Button variant="outline" class="mt-14">
+            Voir les événements
+          </Button>
+        </router-link>
       </div>
     </div>
   </div>
