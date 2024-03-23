1df64e01298595d64d917c31c76dca56d11d41f6
UnMugViolet <jaguinpaul@gmail.com>
2024-03-23 16:24:57 +0100

    Start to work on json parse for projects

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -7,36 +7,48 @@ import project4 from "@/assets/images/archi-affiche.jpg";
 import project5 from "@/assets/images/website-bread.jpg";
 import project6 from "@/assets/images/nft-banner.jpg";
 import project7 from "@/assets/images/festival-env-affiche.jpg";
-import project9 from "@/assets/images/nuit-numerique-affiche.jpg";
+import project8 from "@/assets/images/nuit-numerique-affiche.jpg";
+
+import project from "@/data/projects.json";
+
+const items = ref(projects);
+
+const getItemById = (id) => {
+  return items.value.find(item => item.id === id);
+};
+
+const sortedItems = computed(() => {
+  return [...items.value].sort((a, b) => a.id - b.id);
+});
 </script>
 
 <template>
   <div class="h-screen py-10">
     <div class="grid grid-cols-4 grid-rows-12 gap-8 h-screen">
       <div
-        class="col-start-1 col-end-2 row-start-1 row-end-6 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105"
+        class="col-start-1 col-end-2 row-start-1 row-end-6 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105 cursor-pointer"
         :style="{ backgroundImage: 'url(' + project1 + ')' }"></div>
       <div
-        class="col-start-1 col-end-2 row-start-6 row-end-10 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105"
+        class="col-start-1 col-end-2 row-start-6 row-end-10 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105 cursor-pointer"
         :style="{ backgroundImage: 'url(' + project2 + ')' }"></div>
       <div
-        class="col-start-1 col-end-3 row-start-10 row-end-13 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105"
+        class="col-start-1 col-end-3 row-start-10 row-end-13 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105 cursor-pointer"
         :style="{ backgroundImage: 'url(' + project3 + ')' }"></div>
       <div
-        class="col-start-2 col-end-3 row-start-5 row-end-10 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105"
+        class="col-start-2 col-end-3 row-start-5 row-end-10 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105 cursor-pointer"
         :style="{ backgroundImage: 'url(' + project4 + ')' }"></div>
       <div
-        class="col-start-2 col-end-4 row-start-1 row-end-5 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105"
+        class="col-start-2 col-end-4 row-start-1 row-end-5 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105 cursor-pointer"
         :style="{ backgroundImage: 'url(' + project5 + ')' }"></div>
       <div
-        class="col-start-3 col-end-5 row-start-9 row-end-13 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105"
+        class="col-start-3 col-end-5 row-start-9 row-end-13 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105 cursor-pointer"
         :style="{ backgroundImage: 'url(' + project6 + ')' }"></div>
       <div
-        class="col-start-3 col-end-4 row-start-5 row-end-9 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105"
+        class="col-start-3 col-end-4 row-start-5 row-end-9 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105 cursor-pointer"
         :style="{ backgroundImage: 'url(' + project7 + ')' }"></div>
       <div
-        class="col-start-4 col-end-5 row-start-1 row-end-9 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105"
-        :style="{ backgroundImage: 'url(' + project9 + ')' }"></div>
+        class="col-start-4 col-end-5 row-start-1 row-end-9 rounded-md bg-center bg-cover transition-transform duration-500 ease-in-out transform hover:scale-105 cursor-pointer"
+        :style="{ backgroundImage: 'url(' + project8 + ')' }"></div>
     </div>
   </div>
 </template>
